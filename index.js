5056d86f3b26d63f08e4278e317b760ad4bce984
Olga Vorozheykina <olga.vorozheykina@relexsolutions.com>
2020-12-25 15:06:26 +0200

    Switch icons on click

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const nav = document.querySelector("nav");
 const navButton = document.getElementById("nav-button");
+const closed = document.getElementById("icon-closed");
+const open = document.getElementById("icon-open");
 
 console.log(document.getElementById("nav-button"));
 
@@ -7,8 +9,14 @@ navButton.onclick = function () {
   if (nav.classList.contains("nav-closed")) {
     nav.classList.add("nav-open");
     nav.classList.remove("nav-closed");
+
+    open.classList.remove("hidden");
+    closed.classList.add("hidden");
   } else if (nav.classList.contains("nav-open")) {
     nav.classList.add("nav-closed");
     nav.classList.remove("nav-open");
+
+    open.classList.add("hidden");
+    closed.classList.remove("hidden");
   }
 };
